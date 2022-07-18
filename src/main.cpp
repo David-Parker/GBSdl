a89bda0312e028606fb0a1d7c92db90df7c5a3de
David Parker <davidgparker123@gmail.com>
2022-07-17 19:42:47 -0700

    update settings file, add for emulation

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,14 @@ int main(int argc, char* argv[])
     Json::CharReaderBuilder builder;
     std::string romPath;
     std::ifstream jsonFile;
-    jsonFile.open("connections.json", std::ios::in);
+    jsonFile.open("settings.json", std::ios::in);
 
     if (!jsonFile.is_open())
     {
-        throw std::runtime_error("connections.json file not found!.");
+        throw std::runtime_error("settings.json file not found!.");
     }
 
+    // Read the command line args for the path to the ROM folder.
     if (argc > 1)
     {
         romPath = argv[1];
@@ -35,6 +36,7 @@ int main(int argc, char* argv[])
         std::cin >> romPath;
     }
 
+    // Parse the settings.json file.
     JSONCPP_STRING errs;
     if (!parseFromStream(builder, jsonFile, &root, &errs)) 
     {
@@ -42,12 +44,30 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
+    // The settings allow for the user to force the emulator into either DMG or CGB mode.
+    // Default is to use the value specified in the game cartridge.
+    GameBoy::EMUType emuType;
+
+    if (root["emulatorType"]["forceDMG"].asBool() == true)
+    {
+        emuType = GameBoy::EMUType::DMG;
+    }
+    else if (root["emulatorType"]["forceCGB"].asBool() == true)
+    {
+        emuType = GameBoy::EMUType::CGB;
+    }
+    else 
+    {
+        emuType = GameBoy::EMUType::Cartridge;
+    }
+
     // Inject SDL based handlers for desktop builds.
     GameBoy* boy = new GameBoy(
         "./rom",
         new SDLGraphicsHandler(SCREEN_WIDTH, SCREEN_HEIGHT, SCALE_4X),
         new SDLEventHandler(),
-        new SDLSerialHandler(root["listeningPort"].asInt(), root["clientPort"].asInt(), root["clientIpAddress"].asCString()));
+        new SDLSerialHandler(root["serialConnection"]["listeningPort"].asInt(), root["serialConnection"]["clientPort"].asInt(), root["serialConnection"]["clientIpAddress"].asCString()),
+        emuType);
 
     try
     {
